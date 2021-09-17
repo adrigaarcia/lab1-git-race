dba4074500413e7a98c2432da981dc7eb5696801
adrigaarcia <adriangarciasb@outlook.com>
2021-09-17 18:36:58 +0200

    Minikube demo tutorial

--- scripts/create_image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Download the base image to do our personal image
-docker pull gradle:openj9
-
-# -t option to tag the image as "lab1-git-race"
-docker build -t lab1-git-race .
-
-# Author: Tomás Pelayo - 779691 
